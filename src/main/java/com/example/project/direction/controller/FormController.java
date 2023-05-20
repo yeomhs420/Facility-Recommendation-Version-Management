da974fd79efad03c4f4c4866d88ba64d535eccbb
yeomhs420 <yeomhs420@naver.com>
2023-05-20 23:15:31 +0900

    yeom commit

--- src/main/java/com/example/project/direction/controller/FormController.java
+++ src/main/java/com/example/project/direction/controller/FormController.java
@@ -3,11 +3,14 @@ package com.example.project.direction.controller;
 import com.example.project.direction.dto.InputDto;
 import com.example.project.pharmacy.service.PharmacyRecommendationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,7 +23,7 @@ public class FormController {
         return "main";
     }
 
-    @PostMapping("/search")
+    @GetMapping("/search")
     public ModelAndView postDirection(@ModelAttribute InputDto inputDto){
 
         ModelAndView modelAndView = new ModelAndView();
