1a54d57996ec163963162d0a7e17c214923c8089
miniminitoptop <miniminitoptop@gmail.com>
2023-05-27 01:19:32 +0900

    게시판 추가

---
+++ src/main/java/com/example/project/question/web/controller/PostsApiController.java
@@ -0,0 +1,37 @@
+package com.example.project.question.web.controller;
+
+import com.example.project.question.service.PostsService;
+import com.example.project.question.web.dto.PostsResponseDto;
+import com.example.project.question.web.dto.PostsSaveRequestDto;
+import com.example.project.question.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+
+        return id;
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
