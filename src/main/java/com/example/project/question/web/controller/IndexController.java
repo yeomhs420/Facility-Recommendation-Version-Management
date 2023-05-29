491c2a6f7e6f5f53103925a9e9a9ccdbb1bc1f42
miniminitoptop <miniminitoptop@gmail.com>
2023-05-29 22:17:11 +0900

    게시판 추가

--- src/main/java/com/example/project/question/web/controller/IndexController.java
+++ src/main/java/com/example/project/question/web/controller/IndexController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
 
@@ -18,7 +19,7 @@ public class IndexController {
     private final PostsService postsService;
     private final HttpSession httpSession;
 
-    @GetMapping("/index")
+    @GetMapping("/posts-list")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
 
@@ -28,7 +29,7 @@ public class IndexController {
             model.addAttribute("username", user.getUserName());
         }
 
-        return "index";
+        return "posts-list";
     }
 
     @GetMapping("/posts/save")
@@ -36,6 +37,13 @@ public class IndexController {
         return "posts-save";
     }
 
+    @GetMapping("/posts/view/{id}")
+    public String view(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-view";
+    }
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
